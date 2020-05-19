273c4e845fa816fd6e531698112990a5c8fb8858
mtsealove <40752097+mtsealove@users.noreply.github.com>
2020-05-19 19:31:16 +0900

    css edit, icon change

--- web.js
+++ web.js
@@ -251,7 +251,7 @@ exports.startApp = (port) => {
         });
     });
 
-    /*
+    
     // server
     app.use((req, res, next)=> {
         if(!req.secure) {
@@ -269,12 +269,13 @@ exports.startApp = (port) => {
     http.createServer(app).listen(80);
     https.createServer(options, app).listen(443);
     
-    */
+    
     // local
-   
+   /*
     app.listen(port, () => {
         console.log('web server runings on: ' + port);
     });
+    */
 
     
 }
